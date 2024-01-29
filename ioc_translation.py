b84e296ef1e7698da5ac41f43f098561bd97043f
Viktor Hrebeniuk <76157115+saltar-ua@users.noreply.github.com>
2024-01-29 14:39:25 +0200

    Commit 2

---
+++ ioc_translation.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from app.models.translation import InfoMessage
+
+
+class CTIPlatform(BaseModel):
+    name: str
+
+
+class OneTranslationCTIData(BaseModel):
+    info: Optional[InfoMessage] = None
+    status: bool
+    translations: Optional[list] = None
+    target_siem_type: str
+
+
+class Test2:
+    ...
