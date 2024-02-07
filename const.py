8e18fc595107ad8df895ec6d07bfd9953dbd9dc6
Viktor Hrebeniuk <76157115+saltar-ua@users.noreply.github.com>
2024-02-07 16:09:27 +0200

    Class B3

--- const.py
+++ const.py
@@ -1,15 +1,21 @@
-LOGPOINT_QUERY_DETAILS = {
-    "siem_type": "logpoint",
-    "name": "Logpoint",
-    "group_name": "Logpoint",
-    "platform_name": "Query",
-    "group_id": "logpoint",
+from app.translator.core.models.platform_details import PlatformDetails
+
+UTF8_PAYLOAD_PATTERN = r"UTF8\(payload\)"
+
+QRADAR_QUERY_DETAILS = {
+    "siem_type": "qradar-aql-query",
+    "name": "QRadar Query",
+    "platform_name": "Query (AQL)",
+    "group_id": "qradar",
+    "group_name": "QRadar",
 }
 
+NUM_VALUE_PATTERN = r"(?P<num_value>\d+(?:\.\d+)*)"
+SINGLE_QUOTES_VALUE_PATTERN = r"""'(?P<s_q_value>(?:[:a-zA-Z\*0-9=+%#\-\/\\,_".$&^@!\(\)\{\}\s]|'')*)'"""
 
-class B1:
-    ...
+
+qradar_query_details = PlatformDetails(**QRADAR_QUERY_DETAILS)
 
 
-class B2:
+class B3:
     ...
