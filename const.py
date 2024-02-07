66d77b656da612f4a031e973664668a226e4c762
Viktor Hrebeniuk <76157115+saltar-ua@users.noreply.github.com>
2024-02-07 16:09:15 +0200

    Class B1

---
+++ const.py
@@ -0,0 +1,15 @@
+LOGPOINT_QUERY_DETAILS = {
+    "siem_type": "logpoint",
+    "name": "Logpoint",
+    "group_name": "Logpoint",
+    "platform_name": "Query",
+    "group_id": "logpoint",
+}
+
+
+class B1:
+    ...
+
+
+class B2:
+    ...
