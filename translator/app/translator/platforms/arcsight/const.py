6a973f894cdd3d0c2ff28c13ac1d39b473179564
Viktor Hrebeniuk <76157115+saltar-ua@users.noreply.github.com>
2024-03-06 14:11:06 +0200

    6711: Add few new classes

---
+++ translator/app/translator/platforms/arcsight/const.py
@@ -0,0 +1,11 @@
+ARCSIGHT_QUERY_DETAILS = {
+    "siem_type": "arcsight",
+    "name": "ArcSight Query",
+    "group_name": "ArcSight",
+    "group_id": "arcsight",
+    "platform_name": "Query",
+    "alt_platform_name": "CEF",
+}
+
+class Test:
+    ...
